24e5144fc7684bc47b223b369db58f377c481dc3
MenkeTechnologies <linux.dev25@gmail.com>
2026-07-20 01:57:11 +0000

    deploy: 253847014543040d74d576b2692c0c9c03c36094

--- main/searcher-09f2665d.js
+++ main/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bb35fd40.js',
+                path_to_root + 'searchindex-f9dcfeac.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
