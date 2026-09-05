09d5c29914cd68418ac6804f8ebbd1025369a566
MenkeTechnologies <MenkeTechnologies@users.noreply.github.com>
2026-09-05 23:15:32 +0000

    deploy: b0242c1a0e8174e6d3697c6b338fbae9f7d53870

--- main/searcher-09f2665d.js
+++ main/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f0cfa0fe.js',
+                path_to_root + 'searchindex-d8fc0944.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
